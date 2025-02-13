e5d8f2471687025d3e9c40df35eddac34a345dc9
benoit <benoit.debruyne@nedap.com>
2025-02-13 13:02:26 +0100

    Remove unused lib

--- internal/provider/user_resource.go
+++ internal/provider/user_resource.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
