730b988221e840183c6243ccdd89b3175a93bc87
Luna Stadler <luc@spreadshirt.net>
2026-05-28 14:46:45 +0200

    Make `examples/provider` runnable with `docker compose` setup
    The `demo` name conflicted with the users created by the Ceph demo setup
    in `docker compose`, `dev_test` doesn't exist there by default and works.

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -11,26 +11,34 @@ provider "radosgw" {
   # set access_key_id and secret_access_key via ACCESS_KEY_ID and SECRET_ACCESS_KEY env variables
 }
 
-resource "radosgw_user" "demo_user" {
-  user_id      = "demo"
-  display_name = "Ceph demo user"
+resource "radosgw_user" "dev_test_user" {
+  user_id      = "dev_test"
+  display_name = "Ceph dev_test user"
 }
 
-resource "radosgw_subuser" "demo_subuser_readonly" {
-  user_id = "demo"
+resource "radosgw_subuser" "dev_test_subuser_readonly" {
+  user_id = "dev_test"
   subuser = "readonly"
   access  = "read"
+
+  depends_on = [radosgw_user.dev_test_user]
 }
 
-resource "radosgw_key" "demo_default_key" {
-  user = "demo"
+resource "radosgw_key" "dev_test_default_key" {
+  user = "dev_test"
+
+  depends_on = [radosgw_user.dev_test_user]
 }
 
-resource "radosgw_key" "demo_second_key" {
-  user = "demo"
+resource "radosgw_key" "dev_test_second_key" {
+  user = "dev_test"
+
+  depends_on = [radosgw_user.dev_test_user]
 }
 
-resource "radosgw_key" "demo_readonly_key" {
-  user    = "demo"
+resource "radosgw_key" "dev_test_readonly_key" {
+  user    = "dev_test"
   subuser = "readonly"
+
+  depends_on = [radosgw_subuser.dev_test_subuser_readonly]
 }
