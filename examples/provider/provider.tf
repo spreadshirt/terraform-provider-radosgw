b57436211e055aaf1159cf4d153e077941785e4d
Luna Stadler <luc@spreadshirt.net>
2026-06-03 13:57:33 +0200

    Make it easier to run the examples repeatedly

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -11,13 +11,19 @@ provider "radosgw" {
   # set access_key_id and secret_access_key via ACCESS_KEY_ID and SECRET_ACCESS_KEY env variables
 }
 
+variable "user_suffix" {
+  type        = string
+  description = "suffix to append to generated user"
+  default     = ""
+}
+
 resource "radosgw_user" "dev_test_user" {
-  user_id      = "dev_test"
+  user_id      = "dev_test${var.user_suffix}"
   display_name = "Ceph dev_test user"
 }
 
 resource "radosgw_subuser" "dev_test_subuser_readonly" {
-  user_id = "dev_test"
+  user_id = "dev_test${var.user_suffix}"
   subuser = "readonly"
   access  = "read"
 
@@ -25,19 +31,19 @@ resource "radosgw_subuser" "dev_test_subuser_readonly" {
 }
 
 resource "radosgw_key" "dev_test_default_key" {
-  user = "dev_test"
+  user = "dev_test${var.user_suffix}"
 
   depends_on = [radosgw_user.dev_test_user]
 }
 
 resource "radosgw_key" "dev_test_second_key" {
-  user = "dev_test"
+  user = "dev_test${var.user_suffix}"
 
   depends_on = [radosgw_user.dev_test_user]
 }
 
 resource "radosgw_key" "dev_test_readonly_key" {
-  user    = "dev_test"
+  user    = "dev_test${var.user_suffix}"
   subuser = "readonly"
 
   depends_on = [radosgw_subuser.dev_test_subuser_readonly]
